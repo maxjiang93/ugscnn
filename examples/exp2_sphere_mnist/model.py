718b380a2f7c1ba84809d65f76647db70c54b289
Chiyu Jiang <maxjiang93@gmail.com>
2018-07-12 15:22:37 -0700

    bug fix stride 2

--- examples/exp2_sphere_mnist/model.py
+++ examples/exp2_sphere_mnist/model.py
@@ -8,8 +8,8 @@ class LeNet(nn.Module):
     def __init__(self, device, mesh_folder):
         super(LeNet, self).__init__()
         self.b = 32
-        self.conv1 = MeshConv(1, self.b, mesh_file=os.path.join(mesh_folder, "icosphere_3.pkl"), device=device)
-        self.conv2 = MeshConv(self.b, 2*self.b, mesh_file=os.path.join(mesh_folder, "icosphere_2.pkl"), device=device)
+        self.conv1 = MeshConv(1, self.b, mesh_file=os.path.join(mesh_folder, "icosphere_3.pkl"), device=device, stride=2)
+        self.conv2 = MeshConv(self.b, 2*self.b, mesh_file=os.path.join(mesh_folder, "icosphere_2.pkl"), device=device, stride=2)
         self.csize = 2*self.b*self.conv2.nv_prev
         self.conv2_drop = nn.Dropout2d()
         self.fc1 = nn.Linear(self.csize, 512)
