455546c9b3ae16674041460a7da92620dc537a7a
Chiyu Jiang <maxjiang>
2018-09-05 16:14:09 -0700

    update training script

--- examples/exp2_sphere_mnist/run.sh
+++ examples/exp2_sphere_mnist/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-MESHFILES=../../mesh_files
+source activate
+MESHFILES=../../archive/old_mesh_files
+DATAFILE=mnist_ico4_old_eq.gzip
 
 # assert mesh files exist
 if [ ! -d $MESHFILES ]; then
@@ -9,10 +10,10 @@ if [ ! -d $MESHFILES ]; then
 fi
 
 # generate data
-if [ ! -f mnist_ico4.gzip ]; then
+if [ ! -f $DATAFILE ]; then
     echo "[!] Data files do not exist. Preparing data..."
-    python prepare_data.py --bandwidth 60 --mnist_data_folder raw_data --output_file mnist_ico4.gzip --no_rotate_train --no_rotate_test --mesh_file $MESHFILES/icosphere_4.pkl
+    python prepare_data.py --bandwidth 60 --mnist_data_folder raw_data --output_file $DATAFILE --no_rotate_train --no_rotate_test --mesh_file $MESHFILES/icosphere_4.pkl
 fi
 
 # train
-python train.py --mesh_folder $MESHFILES --datafile mnist_ico4.gzip
+python train.py --mesh_folder $MESHFILES --datafile $DATAFILE --log_dir log_res_rot_adam_deep_ft8 --optim adam --lr 3e-3 --epochs 100 --feat 8 #--dropout #--decay
