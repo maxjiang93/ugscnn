de46b84d6a518bfb298d3b9a62a06dd3e6b8c1a8
Max Jiang <maxjiang93@gmail.com>
2018-09-24 06:22:15 +0800

    run script for baseline

--- baseline/exp5_2d3ds/run.sh
+++ baseline/exp5_2d3ds/run.sh
@@ -5,13 +5,13 @@ python train.py \
 --test-batch-size 16 \
 --epochs 200 \
 --data_folder data_small \
---fold 1 \
---log_dir log/log_fcn_cv1_rgbd \
+--fold 2 \
+--log_dir log/log_fcn_cv2_rgbd \
 --decay \
 --train_stats_freq 5 \
 --model FCN8s \
 --in_ch rgbd \
---lr 3e-3 
+--lr 1e-3 
 
 # FCN8s, UNet, ResNetDUCHDC
 
